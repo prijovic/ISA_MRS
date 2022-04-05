be6f2cba80533773c97877e22fba0fddfbe7e657
Uros <prijovic.uros13@gmail.com>
2022-04-05 16:47:54 +0200

    Inicijalizacija front-end projekta

---
+++ isa_mrs_projekat_front/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <InitialPage/>
+</template>
+
+<script>
+import InitialPage from "@/components/InitialPage";
+
+export default {
+  name: 'App',
+  components: {
+    InitialPage
+  }
+}
+</script>
+
+<style>
+@import "~bootstrap/dist/css/bootstrap.css";
+</style>
