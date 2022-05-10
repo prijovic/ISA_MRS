0cb01eaa5c6f7c50e2bd82193877f078aea9581e
Uros <prijovic.uros13@gmail.com>
2022-05-10 13:09:53 +0200

    Removed path for vacation houses

--- isa_mrs_projekat_front/src/components/UnregisteredUser/TheUnregisteredUserSidebar.vue
+++ isa_mrs_projekat_front/src/components/UnregisteredUser/TheUnregisteredUserSidebar.vue
@@ -2,7 +2,7 @@
   <div class="sidebar rounded-right" :style="{width:sidebarWidth}">
     <MenuLink to="/#" icon="mountain-sun">Adventures</MenuLink>
     <MenuLink to="/#" icon="ship">Boats</MenuLink>
-    <MenuLink to="/vacationRentals" icon="house">Rental Houses</MenuLink>
+    <MenuLink to="/#" icon="house">Rental Houses</MenuLink>
     <span class="collapse-icon" :class="{'rotate-180': collapsed}" @click="toggleSidebar">
       <font-awesome-icon icon="angle-double-left"></font-awesome-icon>
     </span>
